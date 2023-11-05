633d378f95714a6bb2041b63d661814490bab0a8
Varga György Márk <gyorgy.varga@shiwaforce.com>
2023-11-05 23:20:22 +0100

    update

--- src/components/MessageDisplay.vue
+++ src/components/MessageDisplay.vue
@@ -1,14 +1,9 @@
 <template>
   <p class="text-2xl mb-4 text-center">{{ message }}</p>
-  <button @click="alma">Pussh me</button>
 </template>
 
 <script setup lang="ts">
-const props = defineProps({
+defineProps({
   message: String,
 });
-
-const alma = () => {
-  console.log("asdasdasdasd");
-};
 </script>
